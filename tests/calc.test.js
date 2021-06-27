19c581710762028a91b3417164cabe45c7a9edd3
evillt <ijoec123@gmail.com>
2021-06-27 12:44:23 +0800

    test: update

--- tests/calc.test.js
+++ tests/calc.test.js
@@ -15,10 +15,10 @@ test('get decimal precision', (t) => {
 
 test('calc min profit USDT price', (t) => {
   // ETH taker
-  const minProfitUSDTPrice = toCeil(
-    calcMinProfitUSDTPrice(0.002, 0.001, 3.62, 0.1, 0.001)
-  )
-  t.is(minProfitUSDTPrice, 1863.73)
+  // 买入 0.002，手续费率 0.1%，花费 3.62 USDT，最小盈利 0.1 USDT，币种最少买入数量
+  t.is(toCeil(calcMinProfitUSDTPrice(0.002, 0.001, 3.62, 0.1, 0.001)), 1863.73)
+  // 同上，最小盈利变为 1 USDT
+  t.is(toCeil(calcMinProfitUSDTPrice(0.002, 0.001, 3.62, 1, 0.001)), 2314.63)
 })
 
 test('fee ratio', (t) => {
